19da2ad8c352f220c46154ad3b4fa0fa7b7222b9
gaurav10997 <avdahika@gmail.com>
2023-09-12 00:13:24 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import Header from "./components/common/Header"
+import Footer from "./components/common/Footer"
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  )
+}
+export default App
